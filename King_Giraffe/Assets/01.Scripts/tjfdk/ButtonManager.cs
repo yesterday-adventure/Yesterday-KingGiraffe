ab8db04676493fe67c84feb80a840ce3adb76400
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-10-14 12:40:23 +0900

    중간 병합

--- King_Giraffe/Assets/01.Scripts/tjfdk/ButtonManager.cs
+++ King_Giraffe/Assets/01.Scripts/tjfdk/ButtonManager.cs
@@ -17,6 +17,12 @@ public class ButtonManager : MonoBehaviour
     [SerializeField] private GameObject settingPanel;
     [SerializeField] private GameObject settingSavePanel;
 
+
+    [Header("Ranking")]
+    [SerializeField] private bool isRanking = false;
+    [SerializeField] private GameObject rankingPanel;
+    [SerializeField] private GameObject rankingSavePanel;
+
     [Header("GameOver")]
     [SerializeField] private GameObject gameOverPanel;
     [SerializeField] private TextMeshProUGUI gradeTxt1, gradeTxt2, rankTxt, nickTxt;
@@ -39,23 +45,54 @@ public class ButtonManager : MonoBehaviour
         best?.SetActive(false); // 이미지 활성화 초기화
     }
 
-    public void SettingPanel() {
+    public void SettingPanel()
+    {
 
-        if (isSetting) {
-            
+        if (isSetting)
+        {
+
             isSetting = !isSetting; // 현재 판넬 상태 반전
-            settingPanel.transform.DOScale(0.25f, 0.5f).SetEase(Ease.OutExpo).OnComplete(() => {
+            settingPanel.transform.DOScale(0.25f, 0.5f).SetEase(Ease.OutExpo).OnComplete(() =>
+            {
                 settingSavePanel.SetActive(!settingSavePanel.activeSelf); // 보호 판넬 비활성화
-                settingPanel.SetActive(!settingPanel.activeSelf);}); // 판넬 비활성화
+                settingPanel.SetActive(!settingPanel.activeSelf);
+            }); // 판넬 비활성화
         }
-        else {
-            
+        else
+        {
+
             isSetting = !isSetting; // 현재 판넬 상태 반전
-                settingPanel.SetActive(!settingPanel.activeSelf); // 판넬 활성화
-                settingPanel.transform.DOScale(1f, 0.5f).SetEase(Ease.OutExpo).OnComplete(() => {
-                settingSavePanel.SetActive(!settingSavePanel.activeSelf);}); // 보호 판넬 활성화
+            settingPanel.SetActive(!settingPanel.activeSelf); // 판넬 활성화
+            settingPanel.transform.DOScale(1f, 0.5f).SetEase(Ease.OutExpo).OnComplete(() =>
+            {
+                settingSavePanel.SetActive(!settingSavePanel.activeSelf);
+            }); // 보호 판넬 활성화
         }
-        
+
+    }
+
+    public void RankingPanel()
+    {
+
+        if (isRanking)
+        {
+
+            isRanking = !isRanking; // 현재 판넬 상태 반전
+            rankingPanel.transform.DOScale(0.25f, 0.5f).SetEase(Ease.OutExpo).OnComplete(() => {
+                rankingSavePanel.SetActive(!rankingSavePanel.activeSelf); // 보호 판넬 비활성화
+                rankingPanel.SetActive(!rankingPanel.activeSelf);
+            }); // 판넬 비활성화
+        }
+        else
+        {
+
+            isRanking = !isRanking; // 현재 판넬 상태 반전
+            rankingPanel.SetActive(!rankingPanel.activeSelf); // 판넬 활성화
+            rankingPanel.transform.DOScale(1f, 0.5f).SetEase(Ease.OutExpo).OnComplete(() => {
+                rankingSavePanel.SetActive(!rankingSavePanel.activeSelf);
+            }); // 보호 판넬 활성화
+        }
+
     }
 
     public void GameOverPanel() {
